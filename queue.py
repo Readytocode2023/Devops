d6eef9bbb7b20f210bb5e511090dbe78d879f9fc
Readytocode2023 <sahanaa2k3@hotmail.com>
2025-04-23 22:28:54 +0530

    Added enqueue and size methods

--- queue.py
+++ queue.py
@@ -1,3 +1,10 @@
 class Queue:
-	def_init_(self):
-	   self.items=[]
+      
+      def_init_(self):
+	 self.items=[]
+ 	
+
+	def enqueue(self,item):
+	    self.items.append(item)
+	def size(self):
+	    return len(self.items) 

f2a613a7e3e5338cb07acef2e761d082ab62bca7
B1hava2na3 <bhavana.scs24@bmsce.ac.in>
2025-04-24 11:01:45 +0530

    Added dequeue and peek method

--- queue.py
+++ queue.py
@@ -7,4 +7,15 @@ class Queue:
 	def enqueue(self,item):
 	    self.items.append(item)
 	def size(self):
-	    return len(self.items) 
+	    return len(self.items)
+	def is_empty(self):
+	    return len(self.item_==0
+	def dequeue(self):
+	    if self.is_empty():
+		return None
+	    return self.items.pop(0)
+	def peek(self):
+	    if self.is_empty():
+		return None
+	    return self.items[0]
+ 
